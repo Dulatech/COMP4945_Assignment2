03b973d4495633ac37e6bf3c5616f0a280f869ce
Jeavin Shoker <jshokerwow@gmail.com>
2020-01-23 12:43:45 -0800

    Added Plane and Bomb classes

---
+++ COMP4945_Assignment2/Bomb.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+
+namespace COMP4945_Assignment2
+{
+    class Bomb
+    {
+        public PictureBox image;
+        public int Speed = 4;
+        public int Direction { set; get; }
+        public Bomb(int direction, Point location, int player)
+        {
+            image = new PictureBox();
+            image.Size = direction % 2 == 0 ? new Size(10, 18) : new Size(18, 10);
+            image.BackColor = Color.Red;
+            image.Location = location;
+            Direction = direction;
+        }
+        public void Move()
+        {
+            switch(Direction)
+            {
+                case 0: // UP
+                    image.Location = new Point(image.Location.X, image.Location.Y - Speed);
+                    break;
+                case 1: // RIGHT
+                    image.Location = new Point(image.Location.X + Speed, image.Location.Y);
+                    break;
+                case 2: // DOWN
+                    image.Location = new Point(image.Location.X, image.Location.Y + Speed);
+                    break;
+                case 3: // LEFT
+                    image.Location = new Point(image.Location.X - Speed, image.Location.Y);
+                    break;
+            }
+        }
+    }
+}
---
+++ COMP4945_Assignment2/Plane.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP4945_Assignment2
+{
+    class Plane
+    {
+        public PictureBox plane;
+        public int Speed = 10;
+        private int X_Coor { get; set; }
+        private int Y_Coor { get; set; }
+        public int Direction { set; get; }
+
+        public Plane(Point location, int player)
+        {
+            plane = new PictureBox();
+            plane.Size = new Size(50, 50);
+            plane.BackColor = Color.Transparent;
+            plane.Image = Properties.Resources.Plane_Up;
+            plane.Location = location;
+            plane.SizeMode = PictureBoxSizeMode.Zoom;
+            Direction = 0;
+        }
+
+        public void move(int clientH, int clientW, int direction)
+        {
+            switch (direction)
+            {
+                case 0: // UP
+                    if (plane.Top > 0)
+                    {
+
+                        plane.Image = Properties.Resources.Plane_Up;
+                        plane.Location = new Point(plane.Location.X, plane.Location.Y - Speed);
+                    }
+                    break;
+                case 1: // RIGHT
+                    if (plane.Left + plane.Width < clientW)
+                    {
+                        
+
+                        plane.Image = Properties.Resources.Plane_Right;
+                        plane.Location = new Point(plane.Location.X + Speed, plane.Location.Y);
+                    }
+                    break;
+                case 2: // DOWN
+                    if (plane.Top + plane.Height < clientH)
+                    {
+
+                        plane.Image = Properties.Resources.Plane_Down;
+                        plane.Location = new Point(plane.Location.X, plane.Location.Y + Speed);
+                    }
+                    break;
+                case 3: // LEFT
+                    if (plane.Left > 0)
+                    {
+
+
+                        plane.Location = new Point(plane.Location.X - Speed, plane.Location.Y);
+
+                        plane.Image = Properties.Resources.Plane_Left;
+                    }
+
+                    break;
+            }
+        }
+
+
+    }
+}
