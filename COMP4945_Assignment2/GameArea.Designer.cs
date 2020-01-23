9790729ce2704617573de1d3002478add7d8dc2a
MDandaya <MDandaya@gmail.com>
2020-01-23 14:37:26 -0800

    edited gamearea size

--- COMP4945_Assignment2/GameArea.Designer.cs
+++ COMP4945_Assignment2/GameArea.Designer.cs
@@ -41,17 +41,17 @@
             // 
             // GameArea
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.BackgroundImage = global::COMP4945_Assignment2.Properties.Resources._10_tiled_1;
-            this.ClientSize = new System.Drawing.Size(1333, 880);
+            this.ClientSize = new System.Drawing.Size(807, 703);
             this.DoubleBuffered = true;
-            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "GameArea";
             this.Text = "GameArea";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyEvent);
-            this.FormBorderStyle = FormBorderStyle.FixedSingle;
             this.ResumeLayout(false);
 
         }
