e0477bf16a6dc3d9944bbe2d34254479623d0741
Minseok Kong <eoeoms@naver.com>
2020-02-28 15:16:35 -0800

    hub configured

---
+++ SiganlRGameHub/GameHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace SiganlRGameHub
+{
+    public class GameHub : Hub
+    {
+        public void Send(string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.Others.broadcastMessage(message);
+        }
+    }
+}
